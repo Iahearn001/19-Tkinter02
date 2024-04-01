9da1dc8333ebeda7b9c2fbc8518cd847d53c274e
iahearn001 <iahearn001@student.franciscan.edu>
2024-03-31 20:42:04 -0400

    Completed all

--- src/m3_many_widgets.py
+++ src/m3_many_widgets.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   First, create a tkinter window called window.
 #
@@ -15,7 +15,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, we are going to practice all of our widgets.
 #
@@ -39,3 +39,29 @@ import tkinter as tk
 #   
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+window = tk.Tk()
+
+window.title("Widgets Practice I guess")
+
+frame1 = tk.Frame(window)
+frame2 = tk.Frame(window)
+
+frame1.pack(pady=10)
+frame2.pack(pady=10)
+
+label_frame1 = tk.Label(frame1, text="ANYTHING")
+button_frame1 = tk.Button(frame1, text="Do NOT Click Me")
+entry_frame1 = tk.Entry(frame1)
+
+label_frame1.pack()
+button_frame1.pack()
+entry_frame1.pack()
+
+label_frame2 = tk.Label(frame2, text="Please type literally anything")
+text_frame2 = tk.Text(frame2, height=5, width=30)
+
+label_frame2.pack()
+text_frame2.pack()
+
+window.mainloop()
+
